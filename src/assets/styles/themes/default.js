b696726c4fb54af897c91083d4c11ca68e3cfbb8
Michel Oliveira <michel.oliveira999@hotmail.com>
2022-12-19 20:35:52 -0300

    feat: Creating the Header of ContactsList component

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,11 @@
 export default {
-  backgroundColor: '#F6F5Fc',
+  colors: {
+    background: '#F6F5Fc',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5061FC',
+      dark: '#3346F0',
+    },
+  },
 };
