7f25d0429ccfa6cb42118c599577febbe918cb6b
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-01-07 18:42:29 -0300

    feat: Using async/await inside the useEffect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,21 +24,25 @@ export default function Home() {
   )), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contactss?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(
+          `http://localhost:3001/contacts?orderBy=${orderBy}`,
+        );
         await delay(500);
 
         const json = await response.json();
         setContacts(json);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.log('error ', error);
-      })
-      .finally(() => {
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    loadContacts();
   }, [orderBy]);
 
   function handleToggleOrderBy() {
