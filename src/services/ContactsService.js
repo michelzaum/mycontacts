1b40e0963d4ca61050a50a1212f80877ed2b64b5
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-01-10 19:53:12 -0300

    feat: Creating the HttpClient service

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,13 +1,12 @@
-import delay from '../utils/delay';
+import HttpClient from './utils/HttpClient';
 
 class ContactsService {
-  async listContacts(orderBy = 'asc') {
-    const response = await fetch(
-      `http://localhost:3001/contacts?orderBy=${orderBy}`,
-    );
-    await delay(500);
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
 
-    return response.json();
+  async listContacts(orderBy = 'asc') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 }
 
