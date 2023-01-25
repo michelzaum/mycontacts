b2e2db707d00db07ca4bcd15eb8146ead7bc85ff
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-01-24 21:30:43 -0300

    feat: Showing message when contact is not found

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,6 +16,7 @@ import {
   ListHeader,
   ErrorContainer,
   EmptyListContainer,
+  SearchNotFoundContainer,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -23,6 +24,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -126,6 +128,14 @@ export default function Home() {
                 <strong>”Novo contato”</strong> à cima para cadastrar o seu primeiro!
               </p>
             </EmptyListContainer>
+          )}
+          {(contacts.length > 0 && filteredContacts.length < 1) && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier question" />
+              <span>
+                Nenhum resultado foi encontrado para <strong>{searchTerm}</strong>.
+              </span>
+            </SearchNotFoundContainer>
           )}
           {filteredContacts.length > 0 && (
             <ListHeader orderBy={orderBy}>
