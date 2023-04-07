cf48db4370eae4c51c192be4d8e787712d627a84
Michel Oliveira <michel.oliveira999@hotmail.com>
2023-04-07 17:51:59 -0300

    feat: Aborting the first request in useEffect to avoid re-renders using strict mode

--- src/components/ContactForm/useContactForm.js
+++ src/components/ContactForm/useContactForm.js
@@ -40,9 +40,11 @@ export default function useContactForm(onSubmit, ref) {
   }), [setCategories]);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     async function loadCategories() {
       try {
-        const categoriesList = await CategoriesService.listCategories();
+        const categoriesList = await CategoriesService.listCategories(controller.signal);
 
         setCategories(categoriesList);
       } catch {} finally {
@@ -51,6 +53,10 @@ export default function useContactForm(onSubmit, ref) {
     }
 
     loadCategories();
+
+    return () => {
+      controller.abort();
+    };
   }, [setCategories, setIsLoadingCategories]);
 
   function handleNameChange(event) {
--- src/pages/Home/useHome.js
+++ src/pages/Home/useHome.js
@@ -26,13 +26,18 @@ export default function useHome() {
     contact.name.toLowerCase().includes(deferredSearchTerm.toLowerCase())
   )), [contacts, deferredSearchTerm]);
 
-  const loadContacts = useCallback(async () => {
+  const loadContacts = useCallback(async (signal) => {
     try {
       setIsLoading(true);
-      const contactsList = await ContactsService.listContacts(orderBy);
+      const contactsList = await ContactsService.listContacts(orderBy, signal);
+
       setHasError(false);
       setContacts(contactsList);
-    } catch {
+    } catch (error) {
+      if (error instanceof DOMException && error.name === 'AbortError') {
+        return;
+      }
+
       setHasError(true);
       setContacts([]);
     } finally {
@@ -41,7 +46,12 @@ export default function useHome() {
   }, [orderBy]);
 
   useEffect(() => {
-    loadContacts();
+    const controller = new AbortController();
+    loadContacts(controller.signal);
+
+    return () => {
+      controller.abort();
+    };
   }, [loadContacts]);
 
   const handleToggleOrderBy = useCallback(() => {
